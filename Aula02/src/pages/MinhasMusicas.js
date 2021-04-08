46613a9498e53589ecda226623c3ab8b8b7aa239
Ygor-sLima <ygor.lima@bandtec.com.br>
2021-04-08 00:37:46 +0000

    Aula03

---
+++ Aula02/src/pages/MinhasMusicas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MinhasMusicas() {
+    return(
+        <>
+            <h1>AMIGO, AGORA ESTOU AQUI!   :D</h1>
+        </>
+    );
+}
+
+export default MinhasMusicas;
